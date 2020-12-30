dd8df6a3ccc86e5bda8b13902eac272c72aa073a
Al Chang <aac6692@gmail.com>
2020-12-29 20:20:19 -0500

    Initial commit

---
+++ scripts/index.js
@@ -0,0 +1,55 @@
+class TypeWriter {
+  constructor(strings, elementId, loop, speed) {
+    this.strings = strings;
+    this.element = document.getElementById(elementId);
+    this.currentWord = 0;
+    this.isDeleting = false;
+    this.text = "";
+    this.speed = speed;
+    this.loop = loop;
+  }
+
+  type() {
+    let currentLetter = this.text.length;
+    let delay = this.speed;
+
+    if (this.isDeleting) {
+      delay = this.speed / 2;
+      // Case where we are deleting
+      this.text = this.strings[this.currentWord].substring(0, currentLetter - 1);
+      this.element.innerHTML = this.text;
+    } else {
+      delay = this.speed;
+      // Case where we are adding
+      this.text = this.strings[this.currentWord].substring(0, currentLetter + 1);
+      this.element.innerHTML = this.text;
+    }
+
+    if (this.text.length === this.strings[this.currentWord].length) {
+      // If we have typed the whole word, move to next word and start deleting
+      this.isDeleting = true;
+      delay = this.speed * 10;
+      // If we do not want a loop, just end before deleting
+      if (this.currentWord === this.strings.length - 1 && !this.loop) {
+        return;
+      }
+    } else if (this.text.length === 0) {
+      // If we are at the start of the word, start typing
+      delay = this.speed * 10;
+      this.currentWord++;
+
+      this.isDeleting = false;
+      if (this.currentWord === this.strings.length) {
+        this.currentWord = 0;
+      }
+    }
+
+    setTimeout(() => this.type(), delay);
+  }
+}
+
+// When the content is loaded call the function
+document.addEventListener('DOMContentLoaded', function(event) {
+  const writer = new TypeWriter(["student", "software engineer", "data scientist", "runner", "life-long learner"], "buzz-words", false, 100);
+  writer.type();
+})
